08d115542c52aa930f52094eeceab7b9793f873b
Krzysztof Stanaszek <kstanaszek@objectivity.co.uk>
2018-03-04 12:33:11 +0100

    skeleton created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router'
+import { LocationStrategy, HashLocationStrategy, APP_BASE_HREF } from '@angular/common';
 import { AppComponent } from './app.component';
-
+import { AlbumComponent } from './album/album.component';
+import { ArtistComponent } from './artist/artist.component';
+import { SearchComponent } from './search/search.component';
+import { TrackComponent } from './track/track.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlbumComponent,
+    ArtistComponent,
+    SearchComponent,
+    TrackComponent
   ],
   imports: [
     BrowserModule
