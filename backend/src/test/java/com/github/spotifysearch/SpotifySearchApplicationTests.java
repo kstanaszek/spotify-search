04e68bbf99807c4390b3d84f3618cb208b3e9904
Krzysztof Stanaszek <kstanaszek@objectivity.co.uk>
2018-03-07 16:24:36 +0100

    new catalog structure

---
+++ backend/src/test/java/com/github/spotifysearch/SpotifySearchApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.spotifysearch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpotifySearchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
