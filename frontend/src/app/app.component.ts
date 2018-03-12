76e981f3b9d552bd173af14557764ceb39a5d81d
kstanaszek <k.stanaszek@gmail.com>
2018-03-12 21:27:38 +0100

    unused imports removed

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { SpotifyService } from './spotify.service';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +6,4 @@ import { SpotifyService } from './spotify.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
 }
